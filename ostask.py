bfbd3e625dca130b16fa2d884f21db7e38b9a2d2
Samrad <samrat527@gmail.com>
2019-03-20 18:43:06 +0530

    new text file

---
+++ ostask.py
@@ -0,0 +1,36 @@
+import os
+##os.mkdir('code')
+##os.rmdir("code")
+##os.rmdir("hey")
+##for paths, dirs, files in os.walk(r"C:\Users\User\Desktop"):
+## 	print(paths)
+## 	print(dirs)
+## 	print(files)
+
+##path, filename = os.path.split(r"C:\Users\User\Desktop\Purchasing management course")
+##print(path)
+##print(filename)
+
+##filename, extension = os.path.splitext(r"C:\Users\User\Desktop\Purchasing management course\call-for-papers-journal-2019.pdf")
+##print(filename)
+##print(extension)
+
+##pp=input("Enter a path name:")
+##print(os.name)
+##print(os.getcwd())
+##os.mkdir(pp)
+
+
+##check=os.listdir(r"C:\Users\User\Downloads")
+##path,filename=os.path.split(r"C:\Users\User\Downloads")
+##print(path)
+##print(filename)
+
+p=os.listdir(r"C:\Users\User\Desktop\sam")
+for i in p:
+    print(i)
+
+##filename, extension = os.path.splitext(p)
+##print(filename)
+##print(extension)
+
---
+++ test.py
@@ -0,0 +1,16 @@
+import os
+
+##def main():
+##    i = 0
+##      
+##for filename in os.listdir(r"C:\Users\User\Desktop\sam"):
+##    dst ="Hostel" + str(i) + ".jpg"
+##    src ='xyz'+ filename 
+##    dst ='xyz'+ dst 
+##          
+##       
+##    os.rename(src, dst) 
+##    i += 1
+
+a=rename("New Microsoft Excel Worksheet","exe")
+print(a)
